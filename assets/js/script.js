ea6afa3cc6cee07e1f24ed8406aa6a46a9bb1d49
Viki Mulhall <vikimulhall@gmail.com>
2024-09-12 13:16:13 +0000

    Add counterSpan element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//Select elements
+let counterSpan = document.querySelector('.counter span');
+
 /** Add function to retrieve questuibs frin our questions.jon file */
 function retrieveQuestions(){
     let questionRequest = new XMLHttpRequest();
@@ -5,9 +8,11 @@ function retrieveQuestions(){
         if(this.readyState === 4 && this.status === 200) {
             let questions = JSON.parse(this.responseText);
             //questions for each new game
+            let qCount = 10;
         }
     }
     questionRequest.open("GET", "js/questions.json", true);
     questionRequest.send();
 }
 
+retrieveQuestions();
