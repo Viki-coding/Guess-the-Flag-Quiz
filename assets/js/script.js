e6a2d993a602ea793648d1a5b328f50af6837ac0
Viki Mulhall <vikimulhall@gmail.com>
2024-09-12 13:59:24 +0000

    Add variable elements flag img, flag options and correct file path for ireland flag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
 //Select elements
-let counterSpan = document.querySelector('.counter span');
+const counterSpan = document.querySelector('.counter span');
+const divFlagImg = document.querySelector ('flag-img');
+const flagImg = document.querySelector ('.flag-img img');
+const options = document.querySelector ('.options ul');
 
-/** Add function to retrieve questuibs frin our questions.jon file */
-function retrieveQuestions(){
+/** Add function to retrieve questions from our questions.json file */
+function retrieveQuestions() {
     let questionRequest = new XMLHttpRequest();
-    questionRequest.onreadystatechange = function (){
-        if(this.readyState === 4 && this.status === 200) {
+    questionRequest.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
             let questions = JSON.parse(this.responseText);
-            //questions for each new game
+            //number of questions per game
             let qCount = 10;
+            questionNumber(qCount);
         }
     }
     questionRequest.open("GET", "js/questions.json", true);
     questionRequest.send();
+    questionNumber(7) // 
 }
 
 retrieveQuestions();
+
+function questionNumber(num) {
+    counterSpan.innerHTML = num;
+}
